0fdce7e65dbc27a8e5ae3c4d5476c0bf8057c2a0
duqqqq <oguzcanyamac@gmail.com>
2022-05-23 13:33:24 +0300

    a

--- src/pages/3dtest.js
+++ src/pages/3dtest.js
@@ -33,18 +33,18 @@ const RectAreaLightDecl = ({
 export default () => (
     <main>
         <Canvas
-            camera={{ fov: 45, near: 0.1, far: 10000, position: [0, 0, 800] }}
+            camera={{ fov: 45, near: 0.1, far: 5000, position: [0, 0, 800] }}
             orthographic={false}
             style={{ width: '100%' }}
         >
-            <ambientLight intensity={0.2} />
+            <ambientLight intensity={0.7} />
             <RectAreaLightDecl />
             <RectAreaLightDecl
-                intensity={4}
+                intensity={1}
                 width={100}
                 height={1000}
                 position={[0, 0, 2000]}
-                color="purple"
+                color="red"
             />
             <RectAreaLightDecl
                 intensity={0.5}
@@ -62,15 +62,15 @@ export default () => (
             <Model url={GLTF} />
             <Controls
                 autoRotate
-                enablePan={true}
+                enablePan={false}
                 enableZoom={true}
                 enableDamping
                 dampingFactor={0.5}
-                rotateSpeed={1}
+                rotateSpeed={1.2}
                 // maxZoom={2}
                 // minZoom={1}
-                maxPolarAngle={Math.PI / 3}
-                minPolarAngle={Math.PI / 3}
+                // maxPolarAngle={Math.PI / 3}
+                // minPolarAngle={Math.PI / 3}
             />
         </Canvas>
     </main>
