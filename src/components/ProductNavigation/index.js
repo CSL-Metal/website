8d81ee96518fe625b2c9fe1942b153f4fc405ee3
duqqqq <oguzcanyamac@gmail.com>
2022-05-25 13:46:58 +0300

    asd

--- src/components/ProductNavigation/index.js
+++ src/components/ProductNavigation/index.js
@@ -1,14 +1,13 @@
 import React from 'react'
-import Accordion from '@material-ui/core/Accordion';
-import AccordionSummary from '@material-ui/core/AccordionSummary';
-import AccordionDetails from '@material-ui/core/AccordionDetails';
-import Typography from '@material-ui/core/Typography';
-import Link from '@material-ui/core/Link';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import Accordion from '@material-ui/core/Accordion'
+import AccordionSummary from '@material-ui/core/AccordionSummary'
+import AccordionDetails from '@material-ui/core/AccordionDetails'
+import Typography from '@material-ui/core/Typography'
+import Link from '@material-ui/core/Link'
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
 import useNavbarElements from '../useNavbarElements'
 import useTranslations from '../useTranslations'
 import { useLocale } from '../../hooks/locale'
-
 
 function ProductCats(props) {
     const maincat = props.item
@@ -19,33 +18,35 @@ function ProductCats(props) {
     menuElements.map(item => productCategories.push(item.productCategory))
     console.log(productCategories)
     function onlyUnique(value, index, self) {
-        return self.indexOf(value) === index;
+        return self.indexOf(value) === index
     }
     productCategories = productCategories.filter(onlyUnique)
     console.log(productCategories)
-    return (
-        productCategories.map(item => <AccordionDetails >
-            <Accordion >
-                <AccordionSummary
-                    expandIcon={<ExpandMoreIcon />}
-                >
-                    <Typography >{item}</Typography>
+    return productCategories.map(item => (
+        <AccordionDetails>
+            <Accordion>
+                <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                    <Typography>{item}</Typography>
                 </AccordionSummary>
 
-                {menuElements.filter(products => products.productCategory === item).map(item => <AccordionDetails > <Typography>
-                    <Link href={`products/${item.link}`} >
-                        {item.product}
-                    </Link></Typography>                </AccordionDetails>)}
-
-
-
+                {menuElements
+                    .filter(products => products.productCategory === item)
+                    .map(item => (
+                        <AccordionDetails>
+                            {' '}
+                            <Typography>
+                                <Link href={`products/${item.link}`}>
+                                    {item.product}
+                                </Link>
+                            </Typography>{' '}
+                        </AccordionDetails>
+                    ))}
             </Accordion>
-        </AccordionDetails>)
-    )
+        </AccordionDetails>
+    ))
 }
 
 export default function ProductNavigation({ isActive, handleToggleMenu }) {
-
     const { locale } = useLocale()
     let menuElements = useNavbarElements()
     menuElements = menuElements.filter(item => item.lang === locale)
@@ -54,29 +55,21 @@ export default function ProductNavigation({ isActive, handleToggleMenu }) {
     menuElements.map(item => mainCategories.push(item.mainCategory))
 
     function onlyUnique(value, index, self) {
-        return self.indexOf(value) === index;
+        return self.indexOf(value) === index
     }
     mainCategories = mainCategories.filter(onlyUnique)
-
-
-
 
     const { button } = useTranslations()
     return (
         <div style={{ width: '20%' }}>
-            {mainCategories.map(maincat =>
+            {mainCategories.map(maincat => (
                 <Accordion>
-                    <AccordionSummary
-                        expandIcon={<ExpandMoreIcon />}
-                    >
-                        <Typography >{maincat}</Typography>
+                    <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                        <Typography>{maincat}</Typography>
                     </AccordionSummary>
                     <ProductCats item={maincat} />
-
-
-                </Accordion>)}
+                </Accordion>
+            ))}
         </div>
     )
 }
-
-
