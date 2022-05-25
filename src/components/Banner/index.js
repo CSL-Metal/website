4884e66994ddc96f9e69e39ddcceb6991fea21b4
duqqqq <oguzcanyamac@gmail.com>
2022-05-25 13:31:34 +0300

    navbar css

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -1,19 +1,24 @@
 import React from 'react'
 import Slider from 'react-styled-carousel'
+import { LeftArrow } from 'styled-icons/boxicons-regular'
 
-const responsive = [
-    { breakPoint: 1280, cardsToShow: 1 }, // this will be applied if screen size is greater than 1280px. cardsToShow will become 4.
-    { breakPoint: 760, cardsToShow: 1 },
-]
+// const responsive = [
+//     { breakPoint: 1280, cardsToShow: 1 }, // this will be applied if screen size is greater than 1280px. cardsToShow will become 4.
+//     { breakPoint: 760, cardsToShow: 1 },
+// ]
 
 export const Banner = props => {
     return (
         <Slider
-            responsive={responsive}
-            showArrows={false}
+            cardsToShow={1}
+            showArrows={true}
             showDots={true}
             infinite
-            aitoSlide={1500}
+            autoSlide={1500}
+            pauseOnMouseOver
+            hideArrowsOnNoSlides
+            padding={0}
+            LeftArrow={<LeftArrow style={{ width: 50 }} />}
         >
             {props.children}
         </Slider>
